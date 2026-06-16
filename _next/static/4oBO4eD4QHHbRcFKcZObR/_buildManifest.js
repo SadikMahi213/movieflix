9616d4cd8152350d736de25953f3d9fb23fe4fe0
SadikMahi213 <sadikmahi213@gmail.com>
2026-06-16 21:55:18 +0600

    Deploy MovieFlix with basePath fix

--- _next/static/4oBO4eD4QHHbRcFKcZObR/_buildManifest.js
+++ _next/static/4oBO4eD4QHHbRcFKcZObR/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/movieflix//_next/:path+",
+        "destination": "/movieflix/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
